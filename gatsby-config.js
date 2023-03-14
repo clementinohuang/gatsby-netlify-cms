a8affae67622c9f9ee8203654bcce1a2b97b2c4c
clementinohuang <127772245+clementinohuang@users.noreply.github.com>
2023-03-14 15:42:56 +0000

    modified:   gatsby-config.js 	modified:   src/components/Footer.js 	modified:   src/components/all.sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "Parnasse",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+      "At Parnasse, our mission is to elevate the visibility and impact of artists and their work by providing top-tier PR services rooted in a deep understanding of the art world.",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
